eddd808abd82bf4f0c60979a163425594c7bea59
sebastian prado <sebaslkjh@gmail.com>
2022-12-14 17:48:25 -0500

    Añadiendo Todos los Componentes en App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./containers/Header";
+import Companies from "./containers/Companies";
 import Mission from "./containers/Mission";
+import PeopleContainer from "./containers/PeoplesContainer";
+import GetStarted from "./containers/GetStarted";
+import Footer from "./containers/Footer";
 
 const App = () => {
    return (
       <>
          <Header />
+         <Companies />
          <Mission />
+         <PeopleContainer />
+         <GetStarted />
+         <Footer />
       </>
    )
 }
