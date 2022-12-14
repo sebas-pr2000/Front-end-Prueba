dc6e013fca413d9b74ace433409400ebaa668a9d
sebastian prado <sebaslkjh@gmail.com>
2022-12-14 15:18:31 -0500

    Añadiendo Mission en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./containers/Header";
+import Mission from "./containers/Mission";
 
 const App = () => {
    return (
       <>
          <Header />
+         <Mission />
       </>
    )
 }
